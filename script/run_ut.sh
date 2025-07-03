c0b42612eb8427b2debc601eb149bb60db136445
thc <tanghongcheng1@h-partners.com>
2025-07-03 08:28:29 +0000

    !191 增加UT覆盖率统计脚本 * 增加UT覆盖率统计脚本

---
+++ script/run_ut.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+readonly SCRIPT_FULL_PATH=$(pwd)
+readonly PROJECT_FULL_PATH=$(dirname "$SCRIPT_FULL_PATH")
+
+readonly BUILD_PATH="$PROJECT_FULL_PATH/build"
+readonly OUTPUT_PATH="$PROJECT_FULL_PATH/output"
+readonly HYBM_LIB_PATH="$OUTPUT_PATH/hybm/lib"
+readonly COVERAGE_PATH="$OUTPUT_PATH/coverage"
+
+cd ${PROJECT_FULL_PATH}
+rm -rf ${COVERAGE_PATH}
+rm -rf ${BUILD_PATH}
+mkdir -p ${BUILD_PATH}
+cmake -DCMAKE_BUILD_TYPE=DEBUG -DBUILD_TESTS=ON -DBUILD_OPEN_ABI=ON -S . -B ${BUILD_PATH}
+make install -j5 -C ${BUILD_PATH}
+export LD_LIBRARY_PATH=$HYBM_LIB_PATH:$LD_LIBRARY_PATH
+
+set -e
+cd "$OUTPUT_PATH/bin" && ./test_mf_hy --gtest_output=xml:gcover_report/test_detail.xml
+
+mkdir -p "$COVERAGE_PATH"
+cd "$OUTPUT_PATH"
+lcov --d "$BUILD_PATH" --c --output-file "$COVERAGE_PATH"/coverage.info -rc lcov_branch_coverage=1
+lcov -e "$COVERAGE_PATH"/coverage.info "*/smem/*" -o "$COVERAGE_PATH"/coverage.info --rc lcov_branch_coverage=1
+genhtml -o "$COVERAGE_PATH"/result "$COVERAGE_PATH"/coverage.info --show-details --legend --rc lcov_branch_coverage=1
