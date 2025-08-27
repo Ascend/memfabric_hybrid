c840d3dbf3134b6f3d83c7aa5ef297f486c96548
minibao <baoxiaoming@huawei.com>
2025-08-27 12:21:30 +0000

    !627 - added ptracer, a refined version of htracer * clean code * clean code * - added ptracer, a refined version of htracer

---
+++ src/util/csrc/ptracer/ptracer.cpp
@@ -0,0 +1,99 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.
+ */
+#include "ptracer.h"
+#include "ptracer_default.h"
+
+#define PTRACER_API __attribute__((visibility("default")))
+#define PTRACER_VALIDATE_RETURN(CONDITION, MSG, RETURN_VALUE) \
+    do {                                                      \
+        if (!(CONDITION)) {                                   \
+            LastError::Set(MSG);                              \
+            return RETURN_VALUE;                              \
+        }                                                     \
+    } while (0)
+
+static std::mutex g_mutex;
+static bool g_inited = false;
+static int32_t g_tracerType = 0;
+
+namespace ock {
+namespace mf {
+namespace tracer {
+
+int32_t InitDefaultTracer(const std::string &dumpDir, ptracer &tracer)
+{
+#ifdef ENABLE_PTRACER
+    auto &service = DefaultTracer::GetInstance();
+    if (service.StartUp(dumpDir) != 0) {
+        return -1;
+    }
+
+    tracer.trace_begin = DefaultTracer::TraceBegin;
+    tracer.trace_end = DefaultTracer::TraceEnd;
+    tracer.current_time_ns = DefaultTracer::TimeNs;
+    tracer.enabled = true;
+    return 0;
+#else
+    tracer.enabled = false;
+    return 0;
+#endif
+}
+
+void UnInitDefaultTracer(ptracer &tracer)
+{
+#ifdef ENABLE_PTRACER
+    DefaultTracer::GetInstance().ShutDown();
+    tracer.enabled = false;
+    tracer.trace_begin = nullptr;
+    tracer.trace_end = nullptr;
+    tracer.current_time_ns = nullptr;
+#endif
+}
+
+}  // namespace tracer
+}  // namespace mf
+}  // namespace ock
+
+PTRACER_API int32_t ptracer_init(ptracer_config_t *config)
+{
+    std::lock_guard<std::mutex> guard(g_mutex);
+    if (g_inited) {
+        return 0;
+    }
+
+    using namespace ock::mf::tracer;
+    PTRACER_VALIDATE_RETURN(config != nullptr, "invalid config, which is null", -1);
+    PTRACER_VALIDATE_RETURN(config->tracerType == 1, "invalid config.dumpType, only 1 is supported", -1);
+    PTRACER_VALIDATE_RETURN(config->dumpFilePath != nullptr, "invalid param config.dumpFilePath is null", -1);
+
+    int32_t result = 0;
+    if (config->tracerType == 1) {
+        result = InitDefaultTracer(config->dumpFilePath, g_tracer);
+    }
+
+    if (result == 0) {
+        g_tracerType = config->tracerType;
+        g_inited = true;
+    }
+
+    return result;
+}
+
+PTRACER_API void ptracer_uninit()
+{
+    std::lock_guard<std::mutex> guard(g_mutex);
+    if (!g_inited) {
+        return;
+    }
+
+    if (g_tracerType) {
+        ock::mf::tracer::UnInitDefaultTracer(g_tracer);
+    }
+    g_inited = false;
+}
+
+PTRACER_API const char *ptracer_get_last_err_msg()
+{
+    return ock::mf::tracer::LastError::Get();
+}
---
+++ src/util/csrc/ptracer/tracers/ptracer_default.h
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.
+ */
+#ifndef MEM_FABRIC_PTRACER_DEFAULT_H
+#define MEM_FABRIC_PTRACER_DEFAULT_H
+
+#include <condition_variable>
+#include <mutex>
+#include <sstream>
+#include <thread>
+
+#include "ptracer.h"
+#include "ptracer_tracepoint.h"
+
+namespace ock {
+namespace mf {
+namespace tracer {
+/**
+ * @brief A tracer with dump thread thread which periodically dumps tracepoints info into the file,
+ * then view the tracepoints info by 'cat filename'
+ */
+class DefaultTracer {
+public:
+    static void TraceBegin(uint32_t tpId, const char *tpName)
+    {
+        TracepointCollection::TraceBegin(tpId, tpName);
+    }
+
+    static void TraceEnd(uint32_t tpId, uint64_t ns, int32_t goodBad)
+    {
+        TracepointCollection::TraceEnd(tpId, ns, goodBad);
+    }
+
+    static uint64_t TimeNs()
+    {
+        return Monotonic::TimeNs();
+    }
+
+public:
+    static DefaultTracer &GetInstance()
+    {
+        static DefaultTracer tracer;
+        return tracer;
+    }
+
+    int32_t StartUp(const std::string &dumpFileDir);
+    void ShutDown();
+
+public:
+    DefaultTracer(const DefaultTracer &) = delete;
+    DefaultTracer &operator=(const DefaultTracer &) = delete;
+    DefaultTracer(DefaultTracer &&) = delete;
+    DefaultTracer &operator=(DefaultTracer &&) = delete;
+
+private:
+    int PrepareDumpFile(const std::string &dumpDir);
+    void StartDumpThread();
+    void RunInThread();
+    void DumpTracepoints();
+    int GenerateAllTpString(std::stringstream &ss, bool needTotal = false);
+    void GenerateOneTpString(Tracepoint &traceInfo, bool needTotal, std::stringstream &ss, int &traceCount);
+    void WriteTracepoints(std::stringstream &ss);
+    void OverrideWrite(std::stringstream &ss);
+
+    DefaultTracer() = default;
+    ~DefaultTracer() = default;
+
+private:
+    off_t writePos_{0};
+    std::string dumpFilePath_{};
+    std::string headline_{};
+    std::mutex dumpLock_{};
+    bool running_{false};
+    std::thread dumpThread_{};
+    std::condition_variable dumpCond_{};
+
+private:
+    static constexpr int32_t DUMP_PERIOD_SECOND = 10;         /* dump every 10 seconds */
+    static constexpr size_t MAX_DUMP_SIZE = 10 * 1024 * 1024; /* 10MB */
+};
+}  // namespace tracer
+}  // namespace mf
+}  // namespace ock
+
+#endif  // MEM_FABRIC_PTRACER_DEFAULT_H
