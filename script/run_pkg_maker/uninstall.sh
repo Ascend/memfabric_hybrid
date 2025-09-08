0aad560ff678e4a889185445623dfbf2706a2187
zhuyy <zhuyutong4@h-partners.com>
2025-09-08 07:03:12 +0000

    !733 cleancode * cleancode * cleancode

--- script/run_pkg_maker/uninstall.sh
+++ script/run_pkg_maker/uninstall.sh
@@ -15,26 +15,26 @@ function delete_install_files()
 
     install_dir=$1
     print "INFO" "memfabric_hybrid $(basename $1) delete install files!"
-    if [ -d ${install_dir} ]; then
-        chmod -R 700 ${install_dir}
-        rm -rf ${install_dir}
-    elif [ -f ${install_dir} ]; then
+    if [ -f ${install_dir} ]; then
         chmod 700 ${install_dir}
         rm -f ${install_dir}
+    elif [ -d ${install_dir} ]; then
+        chmod -R 700 ${install_dir}
+        rm -rf ${install_dir}
     fi
 }
 
 function delete_latest()
 {
-    cd $1/..
     print "INFO" "memfabric_hybrid delete latest!"
-    if [ -d "latest" ]; then
-        chmod -R 700 latest
-        rm -rf latest
-    fi
+    cd $1/..
     if [ -f "set_env.sh" ]; then
         chmod 700 set_env.sh
         rm -rf set_env.sh
+    fi
+    if [ -d "latest" ]; then
+        chmod -R 700 latest
+        rm -rf latest
     fi
 }
 
@@ -43,10 +43,10 @@ function uninstall_process()
     if [ ! -d $1 ]; then
         return 0
     fi
-    print "INFO" "memfabric_hybrid $(basename $1) uninstall start!"
-    mf_dir=$(cd $1/..;pwd)
+    print "INFO" "memfabric_hybrid $(basename $1) uninstall start.."
     delete_latest $1
     delete_install_files $1
+    mf_dir=$(cd $1/..;pwd)
     if [ -z "$(ls $mf_dir)" ]; then
         chmod -R 700 $mf_dir
         rm -rf $mf_dir
--- src/smem/csrc/config_store/smem_store_factory.cpp
+++ src/smem/csrc/config_store/smem_store_factory.cpp
@@ -250,19 +250,19 @@ std::function<int(const std::string&, char*, size_t&)> StoreFactory::ConvertFunc
     };
 }
 
-int32_t StoreFactory::SetTlsInfo(bool enable, const char *tlsData, const size_t tlsInfoLen) noexcept
+int32_t StoreFactory::SetTlsInfo(bool enable, const char *tlsData, const size_t tlsDataLen) noexcept
 {
     enableTls = enable;
     if (!enable) {
         return StoreErrorCode::SUCCESS;
     }
 
-    if (tlsData == nullptr || tlsInfoLen > MAX_TLS_INFO_LEN) {
+    if (tlsData == nullptr || tlsDataLen > MAX_TLS_INFO_LEN) {
         SM_LOG_ERROR("tls info null or len invalid.");
         return StoreErrorCode::ERROR;
     }
 
-    StoreFactory::tlsInfo = std::string(tlsData, tlsInfoLen);
+    StoreFactory::tlsInfo = std::string(tlsData, tlsDataLen);
     return StoreErrorCode::SUCCESS;
 }
 
