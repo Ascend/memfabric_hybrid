2f8a0ebdb9deb01c7ec31a8a6aeb8852ff45ae57
wangsheng <wangsheng325@huawei.com>
2025-09-23 08:52:54 +0000

    !850 fix copyright * fix copyright fmt * fix copyright

--- example/shm/ShiftPutGet/main.cpp
+++ example/shm/ShiftPutGet/main.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) Huawei Technologies Co., Ltd. 2022-2023. All rights reserved.
+ * Copyright (c) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.
  * This file constains code of cpu debug and npu code.We read data from bin file
  * and write result to file.
  */
--- script/run_pkg_maker/run_header.sh
+++ script/run_pkg_maker/run_header.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-# Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
-lines=44  # 本文件行数，行尾空行不能删
+# Copyright (c) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.
+# This file is a part of the CANN Open Software.
+# Licensed under CANN Open Software License Agreement Version 1.0 (the "License").
+# Please refer to the License for details. You may not use this file except in compliance with the License.
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+# See LICENSE in the root of the software repository for the full text of the License.
+lines=50  # 本文件行数，行尾空行不能删
 
 SCRIPT_PATH=$(readlink -f "$0") || { echo "readlink failed"; exit 1; }
 TMP_DIR=$(dirname "$SCRIPT_PATH")
