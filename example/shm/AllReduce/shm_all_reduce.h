19214c79c7e7a17c42b9c3bb7c2ef20d658b3bbb
j00808874 <jiangchanghong3@huawei.com>
2025-12-03 18:50:28 +0800

    版本头文件替换
    Co-authored-by: j00808874<jiangchanghong3@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !24 merge mr2 into develop
    
    版本头文件替换
    
    Created-by: j00808874
    Commit-by: j00808874
    Merged-by: chenyz6
    Description: 版本头文件替换
    
    See merge request: Ascend/memfabric_hybrid!24

--- example/shm/AllReduce/shm_all_reduce.h
+++ example/shm/AllReduce/shm_all_reduce.h
@@ -1,8 +1,14 @@
 /*
  * Copyright (c) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.
- * This file constains code of cpu debug and npu code. We read data from bin file
- * and write result to file.
- */
+ * MemFabric_Hybrid is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+*/
 #ifndef SHM_ALL_REDUCE_H
 #define SHM_ALL_REDUCE_H
 
