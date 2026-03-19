0c1cf711a984a8bb18bb9ae48261ed1de0569c2b
p3rry <penghaiqing1@huawei.com>
2026-03-19 11:42:08 +0800

    transport/host UT
    Co-authored-by: p3rry<penghaiqing1@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !422 merge develop_0319hostUT into develop
    
    transport/host UT
    
    Created-by: p3rry
    Commit-by: p3rry
    Merged-by: yrewzjsx
    Description: # Pull Request Template
    
    ## Description
    
    <!-- Provide a clear summary of the change, the problem it solves, and the technical approach. -->
    
    ## Related Issues
    
    <!-- Replace with actual issue numbers. Example: Closes #123, Related to #456 -->
    
    ## Type of Change
    
    - [ ] Bug fix
    - [ ] New feature
    - [ ] Performance optimization
    - [ ] Documentation update
    - [ ] Other (please describe):_______________
    
    ## Testing
    
    <!-- Describe how the changes were tested. Include:
    - Unit/integration test coverage
    - Manual validation steps
    - Links to CI reports (e.g., CIDA) if available
    - Note any pending tests
    -->
    ![image.png](https://raw.gitcode.com/user-images/assets/7672916/ccd13e74-c654-4b5b-aa6e-693c8d397a95/image.png 'image.png')
    ![image.png](https://raw.gitcode.com/user-images/assets/7672916/b6f3b5ba-385b-4c5c-9d18-f555bc2d56bd/image.png 'image.png')
    ## Checklist
    
    - [ ] I have performed a self-review of my own code.  
    - [ ] I have updated the documentation.  
    - [ ] I have added tests to prove my changes are effective.
    
    See merge request: Ascend/memfabric_hybrid!422

--- script/run_ut.sh
+++ script/run_ut.sh
@@ -104,7 +104,6 @@ if ! $FAST_MODE; then
             "*/hybm/csrc/ts_engine/*"
             "*/hybm/csrc/under_api/*"
             "*/hybm/csrc/transport/device/*"
-            "*/hybm/csrc/transport/host/*"
             "*/util/csrc/ptracer/tracers/*"
     )
     lcov -d "$BUILD_PATH" --c --output-file "$COVERAGE_PATH"/coverage.info -rc lcov_branch_coverage=1 --rc lcov_excl_br_line="LCOV_EXCL_BR_LINE|SM_LOG*|SM_ASSERT*|BM_LOG*|BM_ASSERT*|SM_VALIDATE_*|ASSERT*|LOG_*"
