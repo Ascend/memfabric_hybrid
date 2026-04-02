e20aac54dd1e2e39b16e4560e693c92c297a6b7a
shilinlee_com <836160610@qq.com>
2026-04-02 16:41:38 +0800

    chore: memfabric only support >= py38 for manylinux 2.28
    Co-authored-by: shilinlee<836160610@qq.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !491 merge mf_py38 into develop
    
    chore: memfabric only support >= py38 for manylinux 2.28
    
    Created-by: shilinlee_com
    Commit-by: shilinlee
    Merged-by: chenyz6
    Description: # Pull Request Template
    
    ## Description
    
    chore: memfabric only support >= py38 for manylinux 2.28
    
    ## Related Issues
    
    <!-- Replace with actual issue numbers. Example: Closes #123, Related to #456 -->
    
    ## Type of Change
    
    - [ ] Bug fix
    - [ ] New feature
    - [ ] Performance optimization
    - [ ] Documentation update
    - [ ] Other (please describe):_______________
    
    ## Testing
    
    ### 非manylinux版本
    
    | py_version | 状态 | wheel 结构 |
      |---|---|---|
      | cp38-cp38 | SUCCESS | memfabric_hybrid-1.1.0+cpu-cp38-cp38-linux_aarch64.whl |
      | cp39-cp39 | SUCCESS | memfabric_hybrid-1.1.0+cpu-cp39-cp39-linux_aarch64.whl |
      | cp310-cp310 | SUCCESS | memfabric_hybrid-1.1.0+cpu-cp310-cp310-linux_aarch64.whl |
      | cp311-cp311 | SUCCESS | memfabric_hybrid-1.1.0+cpu-cp311-cp311-linux_aarch64.whl |
      | cp312-cp312 | SUCCESS | memfabric_hybrid-1.1.0+cpu-cp312-cp312-linux_aarch64.whl |
      | cp313-cp313 | SUCCESS | memfabric_hybrid-1.1.0+cpu-cp313-cp313-linux_aarch64.whl |
      | cp314-cp314 | SUCCESS | memfabric_hybrid-1.1.0+cpu-cp314-cp314-linux_aarch64.whl |
    
    
    ### manylinux 版本
    
    
     | py_version | python -V | build_exit | wheel_files | status |
      |---|---|---:|---|---|
      | cp38-cp38 | Python 3.8.20 | 0 | memfabric_hybrid-1.1.0+cpu-cp38-cp38-manylinux_2_26_aarch64.manylinux_2_28_aarch64.whl | SUCCESS |
      | cp39-cp39 | Python 3.9.25 | 0 | memfabric_hybrid-1.1.0+cpu-cp39-cp39-manylinux_2_26_aarch64.manylinux_2_28_aarch64.whl | SUCCESS |
      | cp310-cp310 | Python 3.10.20 | 0 | memfabric_hybrid-1.1.0+cpu-cp310-cp310-manylinux_2_26_aarch64.manylinux_2_28_aarch64.whl | SUCCESS |
      | cp311-cp311 | Python 3.11.15 | 0 | memfabric_hybrid-1.1.0+cpu-cp311-cp311-manylinux_2_26_aarch64.manylinux_2_28_aarch64.whl | SUCCESS |
      | cp312-cp312 | Python 3.12.13 | 0 | memfabric_hybrid-1.1.0+cpu-cp312-cp312-manylinux_2_26_aarch64.manylinux_2_28_aarch64.whl | SUCCESS |
      | cp313-cp313 | Python 3.13.12 | 0 | memfabric_hybrid-1.1.0+cpu-cp313-cp313-manylinux_2_26_aarch64.manylinux_2_28_aarch64.whl | SUCCESS |
      | cp314-cp314 | Python 3.14.3 | 0 | memfabric_hybrid-1.1.0+cpu-cp314-cp314-manylinux_2_26_aarch64.manylinux_2_28_aarch64.whl | SUCCESS |
    
    ![image.png](https://raw.gitcode.com/user-images/assets/7672916/5185c37a-fdf1-4596-ad8b-5bb7bccff532/image.png 'image.png')
    
    ## Checklist
    
    - [x] I have performed a self-review of my own code.  
    - [ ] I have updated the documentation.  
    - [ ] I have added tests to prove my changes are effective.
    
    See merge request: Ascend/memfabric_hybrid!491

--- src/smem/python/memfabric_hybrid/setup.py
+++ src/smem/python/memfabric_hybrid/setup.py
@@ -12,16 +12,14 @@
 
 """python api for memfabric_hybrid."""
 
-import os
-import sys
-import platform
 import glob
-import shutil
+import os
+import platform
 import subprocess
-from setuptools import Extension, setup
-from setuptools import find_namespace_packages
+import sys
+
+from setuptools import find_namespace_packages, setup
 from setuptools.dist import Distribution
-from setuptools.command.build_ext import build_ext
 from wheel.bdist_wheel import bdist_wheel
 
 
@@ -31,20 +29,11 @@ def check_env_flag(name: str, default: str = "") -> bool:
 
 # 消除whl压缩包的时间戳差异
 os.environ["SOURCE_DATE_EPOCH"] = "0"
-
 current_version = os.getenv("MEMFABRIC_VERSION")
 if not current_version:
     print("Error: MEMFABRIC_VERSION environment variable must be set.", file=sys.stderr)
 is_manylinux = check_env_flag("IS_MANYLINUX", "FALSE")
-build_open_abi = os.getenv("BUILD_OPEN_ABI", "OFF")
-build_mode = os.getenv("BUILD_MODE", "RELEASE")
-enable_ptracer = os.getenv("ENABLE_PTRACER", "ON")
 xpu_type = os.getenv("XPU_TYPE", "NPU")
-build_hcom = os.getenv("MF_BUILD_HCOM", "OFF")
-build_hcom_with_rdma = os.getenv("MF_BUILD_HCOM_WITH_RDMA", "ON")
-build_hcom_with_ub = os.getenv("MF_BUILD_HCOM_WITH_UB", "OFF")
-build_etcd_backend = os.getenv("BUILD_ETCD_BACKEND", "OFF")
-build_tool = os.getenv("BUILD_TOOL", "cmake")
 
 if xpu_type not in ("NPU", "NONE", "GPU"):
     raise ValueError("XPU_TYPE must be exactly NPU, NONE, or GPU")
@@ -53,8 +42,6 @@ if xpu_type == "NONE":
 elif xpu_type == "GPU":
     current_version += "+gpu"
 
-if build_tool not in ("cmake", "bazel"):
-    raise ValueError("BUILD_TOOL must be exactly cmake or bazel")
 
 class BinaryDistribution(Distribution):
     """Distribution which always forces a binary package with platform name"""
@@ -86,103 +73,6 @@ class BuildWheel(bdist_wheel):
             os.remove(file)
 
 
-class CMakeBuildExt(build_ext):
-    def run(self):
-        root_dir = os.path.abspath(
-            os.path.join(os.path.dirname(__file__), "../../../../")
-        )
-        build_dir = os.path.abspath(os.path.join(root_dir, "build"))
-        install_dir = os.path.abspath(os.path.join(build_dir, "install"))
-        os.makedirs(build_dir, exist_ok=True)
-        config_mode = "Release"
-        if build_mode == "DEBUG":
-            config_mode = "Debug"
-        if build_tool == "cmake":
-            subprocess.check_call(
-                [
-                    "cmake",
-                    f"-S{root_dir}",
-                    f"-B{build_dir}",
-                    f"-DCMAKE_INSTALL_PREFIX={install_dir}",
-                    f"-DCMAKE_BUILD_TYPE={build_mode}",
-                    f"-DBUILD_OPEN_ABI={build_open_abi}",
-                    f"-DENABLE_PTRACER={enable_ptracer}",
-                    f"-DXPU_TYPE={xpu_type}",
-                    "-DBUILD_PYTHON=ON",
-                    "-DBUILD_UT=OFF",
-                ]
-            )
-            subprocess.check_call(
-                [
-                    "cmake",
-                    "--build",
-                    build_dir,
-                    "--config",
-                    config_mode,
-                    "--target",
-                    "install",
-                    "-j8",
-                ]
-            )
-        else:
-            bazel_cmd = ["bazel", "build", "//src/..."]
-            if build_mode == "DEBUG":
-                bazel_cmd.append("--compilation_mode=dbg")
-            else:
-                bazel_cmd.append("--compilation_mode=opt")
-
-            if (build_open_abi == "ON"):
-                bazel_cmd.append("--copt=-D_GLIBCXX_USE_CXX11_ABI=1")
-            else:
-                bazel_cmd.append("--copt=-D_GLIBCXX_USE_CXX11_ABI=0")
-
-            if (enable_ptracer == "ON"):
-                bazel_cmd.append("--copt=-DENABLE_PTRACER")
-
-            if xpu_type == "NPU":
-                bazel_cmd.append("--copt=-DASCEND_NPU")
-            elif xpu_type == "GPU":
-                bazel_cmd.append("--copt=-DNVIDIA_GPU")
-            else:
-                bazel_cmd.append("--copt=-DNO_XPU")
-
-            if build_hcom == "ON":
-                bazel_cmd.append("--define=build_with_hcom=1")
-    
-                if build_hcom_with_rdma == "OFF":
-                    bazel_cmd.append("--define=hcom_enable_rdma=0")
-                if build_hcom_with_ub == "ON":
-                    bazel_cmd.append("--define=hcom_enable_ub=1")
-
-            if build_etcd_backend == "OFF":
-                bazel_cmd.append("--build_tag_filters=-enable_etcd_client")
-            
-            bazel_cmd.append("--explain=explain.log")
-            bazel_cmd.append("--verbose_explanations")
-
-            print(bazel_cmd)
-            subprocess.check_call(
-                bazel_cmd
-            )
-        super().run()
-
-    def build_extension(self, ext):
-        print("skip the step cause _pymf_hybrid is already builded")
-        root_dir = os.path.join(os.path.dirname(__file__), "..")
-        install_dir = os.path.join(root_dir, "build", "install")
-        lib_dir = os.path.join(install_dir, "lib")
-        include_dir = os.path.join(install_dir, "include")
-
-        ext.extra_compile_args = ["-std=c++11"]
-        ext.include_dirs.append(include_dir)
-        ext.library_dirs.append(lib_dir)
-        ext.libraries.append("mf_smem")
-        super().build_extension(ext)
-        ext_dir = os.path.dirname(self.get_ext_fullpath(ext.name))
-        shutil.copy(os.path.join(lib_dir, "libmf_smem.so"), ext_dir)
-        shutil.copy(os.path.join(lib_dir, "libmf_hybm_core.so"), ext_dir)
-
-
 pkgs = find_namespace_packages()
 print(pkgs)
 
@@ -195,7 +85,7 @@ setup(
     packages=find_namespace_packages(exclude=("tests*",)),
     url="https://gitcode.com/Ascend/memfabric_hybrid",
     license="Mulan PSL v2",
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     zip_safe=False,
     package_data={
         "memfabric_hybrid": [
@@ -209,7 +99,6 @@ setup(
         ]
     },
     cmdclass={
-        "build_ext": CMakeBuildExt,
         "bdist_wheel": BuildWheel,
     },
     distclass=BinaryDistribution,
