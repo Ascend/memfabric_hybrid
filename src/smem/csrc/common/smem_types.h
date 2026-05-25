001d4b9434d96dbf79bd92707a855508be8513af
mrh1024 <marunhua1@h-partners.com>
2026-05-25 21:26:01 +0800

    [feature] 修改Join超时时间为600s
    Co-authored-by: mrh1024<marunhua1@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !741 merge develop_fix_join into develop
    
    [feature] 修改Join超时时间为600s
    
    Created-by: mrh1024
    Commit-by: mrh1024
    Merged-by: yrewzjsx
    Description: # Pull Request Template
    
    ## Description
    
    <!-- Provide a clear summary of the change, the problem it solves, and the technical approach. -->
    
    ## Related Issues
    
    <!-- Replace with actual issue numbers. Example: Closes #123, Related to #456 -->
    
    ## Type of Change
    
    - [ ] Bug fix
    - [ ] New feature
    - [ ] Performance optimization
    - [ ] Documentation update
    - [ ] Other (please describe):_______________
    
    ## Testing
    
    <!-- Describe how the changes were tested. Include:
    - Unit/integration test coverage
    - Manual validation steps
    - Links to CI reports (e.g., CIDA) if available
    - Note any pending tests
    -->
    
    ## Checklist
    
    - [ ] I have performed a self-review of my own code.  
    - [ ] I have updated the documentation.  
    - [ ] I have added tests to prove my changes are effective.
    
    See merge request: Ascend/memfabric_hybrid!741

--- src/smem/csrc/common/smem_types.h
+++ src/smem/csrc/common/smem_types.h
@@ -61,7 +61,7 @@ constexpr uint64_t HYBM_MAX_POOL_SIZE = 128ULL << 40;     // 128T, 910C暂时只
 constexpr uint64_t HYBM_HBM_SIZE_ALIGNMENT = 1ULL << 30;  // 1GB
 
 constexpr uint32_t SMEM_GROUP_RETRY_TIME = 5U;
-constexpr uint32_t MF_GROUP_JOIN_DEFAULT_TIMEOUT = 60U;   // 集群加入默认超时时间60s
+constexpr uint32_t MF_GROUP_JOIN_DEFAULT_TIMEOUT = 600U;   // 集群加入默认超时时间600s
 } // namespace smem
 } // namespace ock
 
